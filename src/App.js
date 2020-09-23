c01290246152af87fb40b1a3cccdda2b665d3eff
Andrius <andrius.rochalazarino@gmail.com>
2020-09-23 10:45:03 -0400

    Formulários e botão criado

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+const Main = styled.form`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
+const Forms = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+const ButtonCustom = styled.button`
+  margin-top: 3%;
+`
 
 function App() {
+  const [inputWeight,setInputWeight] = useState("")
+  const [inputHeight,setInputHeight] = useState("")
+
+
+  const handleInputWeight = (e)=>{
+    setInputWeight(e.target.value)
+  }
+  const handleInputHeight = (e)=>{
+    setInputHeight(e.target.value)
+  }
+  
+  const handleSubmit = (e)=>{
+    e.preventDefault()
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <Main onSubmit={handleSubmit}>
+      <header>
+        <h2>Calculo do IMC</h2>
       </header>
-    </div>
+      <Forms>
+        <h4>Digite sua altura:</h4>
+        <input type="text" value={inputWeight} onChange={handleInputWeight} />
+        <h4>Digite seu peso:</h4>
+        <input type="text" value={inputHeight} onChange={handleInputHeight} />
+      </Forms>
+      <ButtonCustom type="submit" >Calcular</ButtonCustom>
+    </Main>
   );
 }
 
